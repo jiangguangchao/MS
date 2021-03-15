9dfcc809d37a3d7adc8ba079d159ee1ccc67a1d2
jgc <jiangguangchao157@163.com>
2021-03-15 13:57:56 +0800

    订单管理后台项目，目前仅包换简单的登录测试和跨域问题解决

---
+++ src/main/java/com/main/util/WebConfig.java
@@ -0,0 +1,23 @@
+package com.main.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @description:
+ * @author:jgc
+ * @create:2021-03-15 13:07
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("Http://localhost:8080","null")
+                .allowedMethods("GET","POST","PUT","OPTIONS","DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
