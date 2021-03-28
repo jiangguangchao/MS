a6cb7fe5d98b4d1046169762b7a799bc35f532bf
jgc_note <jiangguangchao157@163.com>
2021-03-28 14:17:27 +0800

    修复问题：实体类User中的属性[role]类型改为Integer，以便和数据库保持一致

--- src/main/java/com/main/bean/User.java
+++ src/main/java/com/main/bean/User.java
@@ -9,7 +9,7 @@ public class User {
 
     private String userId;
     private String password;
-    private String role;
+    private Integer role;
     private String state;
 
     public String getUserId() {
@@ -28,11 +28,11 @@ public class User {
         this.password = password;
     }
 
-    public String getRole() {
+    public Integer getRole() {
         return role;
     }
 
-    public void setRole(String role) {
+    public void setRole(Integer role) {
         this.role = role;
     }
 
