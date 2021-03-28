ea2e6a16e41e0d55c641dd8f3a0df79aec5f260f
jgc_note <jiangguangchao157@163.com>
2021-03-28 14:40:57 +0800

    回滚上个版本

--- src/main/resources/sql/f_order.sql
+++ src/main/resources/sql/f_order.sql
@@ -5,9 +5,9 @@ create table if not exists f_order(
     furniture varchar(100) comment '家具品类',
     totalAmount decimal(7,2) comment '总金额',
     frontMoney decimal(7,2) comment '定金',
-    frontMoneyTime timestamp default CURRENT_TIMESTAMP comment '定金支付时间',
-    payTime timestamp default CURRENT_TIMESTAMP comment '余款支付时间',
-    deliverTime timestamp default CURRENT_TIMESTAMP comment '送货时间',
+    frontMoneyTime timestamp comment '定金支付时间',
+    payTime timestamp comment '余款支付时间',
+    deliverTime timestamp comment '送货时间',
     state enum('1','2','3','4'),
     createTime timestamp default CURRENT_TIMESTAMP comment '创建时间'
 );
